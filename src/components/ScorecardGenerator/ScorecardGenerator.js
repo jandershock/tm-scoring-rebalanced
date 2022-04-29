2d92324fa29483998a936f02c077f4639c0191bd
jandershock <jordan.andershock@gmail.com>
2022-04-29 10:38:04 -0500

    Added placeholder save and print buttons. Added spacing at bottom of page.

--- src/components/ScorecardGenerator/ScorecardGenerator.js
+++ src/components/ScorecardGenerator/ScorecardGenerator.js
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap"
+import { Col, Container, Row, Button } from "react-bootstrap"
 import { AwardsList } from "./Awards/AwardsList/AwardsList"
 import { MilestonesList } from "./Milestones/MilestonesList/MilestonesList"
 
@@ -19,7 +19,18 @@ export const ScorecardGenerator = () => {
                         <AwardsList />
                     </Col>
                 </Row>
+                <Row className="justify-content-end mt-5">
+                    <Col md="2">
+                        <Button className="w-100" disabled variant="success" type="button">Save</Button>
+                    </Col>
+                    <Col md="2">
+                        <Button className="w-100" disabled variant="primary" type="button">Print</Button>
+                    </Col>
+                </Row>
             </Container>
+            <br/>
+            <hr/>
+            <br/>
         </>
     )
 }
