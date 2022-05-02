84ee4cca5533ce8adc70bfe01db5a855c1b0198c
jandershock <jordan.andershock@gmail.com>
2022-05-02 18:19:10 -0500

    Login and logout now works.

--- src/components/TMScoringRebalanced.js
+++ src/components/TMScoringRebalanced.js
@@ -1,12 +1,24 @@
+import { useState } from "react"
 import { ApplicationViews } from "./ApplicationViews"
 import { NavbarWrapper } from "./nav/NavbarWrapper"
 
 export const TMScoringRebalanced = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(sessionStorage.getItem("tm_ScoringRebalanced_user") !== null)
+
+    const setAuthUser = (user) => {
+        sessionStorage.setItem("tm_ScoringRebalanced_user", JSON.stringify(user))
+        setIsAuthenticated(sessionStorage.getItem("tm_ScoringRebalanced_user") !== null)
+    }
+    
+    const clearUser = () => {
+        sessionStorage.clear();
+        setIsAuthenticated(sessionStorage.getItem("tm_ScoringRebalanced_user") !== null)
+      }
 
     return (
         <>
-            <NavbarWrapper />
-            <ApplicationViews />
+            <NavbarWrapper isAuthenticated={isAuthenticated} clearUser={clearUser} />
+            <ApplicationViews setAuthUser={setAuthUser} />
         </>
     )
 }
--- src/components/nav/NavbarWrapper.js
+++ src/components/nav/NavbarWrapper.js
@@ -1,11 +1,31 @@
-import { Navbar } from "react-bootstrap"
+import { Nav, Navbar } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
-export const NavbarWrapper = () => {
+export const NavbarWrapper = ({ isAuthenticated, clearUser }) => {
 
+    const displayLinks = (authBool) => {
+        if (!authBool) {
+            return (
+                <>
+                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                    <Nav.Link as={Link} to="/registers">Register</Nav.Link>
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <Nav.Link as={Link} to="/scorecards">Saved Scorecards</Nav.Link>
+                    <Nav.Link as={Link} to="/" onClick={clearUser}>Logout</Nav.Link>
+                </>
+            )
+        }
+    }
     return (
         <Navbar bg="dark" variant="dark">
-
             <Navbar.Brand className="px-3">Hello Mars!</Navbar.Brand>
+            <Navbar.Collapse className="justify-content-end px-3">
+                {displayLinks(isAuthenticated)}
+            </Navbar.Collapse>
         </Navbar>
     )
 }
